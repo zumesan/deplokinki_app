b67ee48d2fe6c11ee282ce9abcd655e39794b4ba
zumesan <zumehashi3240518@icloud.com>
2022-09-24 02:24:09 +0900

    Userモデルに渡すパラメーターを許可するメソッドを、コントローラーに記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseに関するコントローラーの処理の場合にメソッドを実行する
+
+  private
+  def configure_permitted_parameters
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number])
+  end
 end
