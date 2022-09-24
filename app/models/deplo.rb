aff32cfb01319aa40dd91444b9d969fbc37373ef
zumesan <zumehashi3240518@icloud.com>
2022-09-24 17:50:25 +0900

    deploバリデーションの設定、prefectureモデルの編集

--- app/models/deplo.rb
+++ app/models/deplo.rb
@@ -5,6 +5,8 @@ class Deplo < ApplicationRecord
   belongs_to :category
   belongs_to :prefecture
 
-
+  validates :deplo_title, :deplo_info, :municipality, presence: true
+  validates :category_id, numericality: { greater_than_or_equal_to:1 ,less_than_or_equal_to:6, message: "is invalid" }
+  validates :prefecture_id, numericality: { greater_than_or_equal_to:1 ,less_than_or_equal_to:7, message: "is invalid" }
 
 end
