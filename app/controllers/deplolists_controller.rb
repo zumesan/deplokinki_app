aa4d72a5feff3d03ca27cfa483be5195a9550428
zumesan <zumehashi3240518@icloud.com>
2022-09-25 17:22:35 +0900

    beforeアクション、privateメソッドの設定

--- app/controllers/deplolists_controller.rb
+++ app/controllers/deplolists_controller.rb
@@ -1,4 +1,16 @@
 class DeplolistsController < ApplicationController
+  before_action :set_deplo, except: [:index]
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+  
+  private
+  def set_deplo
+    @deplo = Deplo.find(params[:id])
+  end
+
+  def deplo_params
+    params.require(:deplo).permit(:deplo_title, :deplo_info, :municipality, :category_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
