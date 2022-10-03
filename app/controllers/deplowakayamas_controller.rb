9e9e00cb3de1decfce0255eb3703219b38975a04
zumesan <zumehashi3240518@icloud.com>
2022-10-03 15:16:05 +0900

    wakayamasコントローラーの編集

--- app/controllers/deplowakayamas_controller.rb
+++ app/controllers/deplowakayamas_controller.rb
@@ -1,4 +1,16 @@
 class DeplowakayamasController < ApplicationController
+  before_action :set_deplo, except: [:index]
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @wakayamas = Deplo.where(prefecture_id: 7)
+  end
+
+  private
+  def set_deplo
+    @deplo = Deplo.find(params[:deplo_id])
+  end
+  def deplo_params
+    params.require(:deplo).permit(:deplo_title, :deplo_info, :municipality, :category_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
