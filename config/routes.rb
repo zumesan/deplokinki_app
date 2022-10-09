e9f1c0a8231f62edeb8be95cf948b797a2671eb2
zumesan <zumehashi3240518@icloud.com>
2022-10-09 13:55:01 +0900

    updateルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "deplos#index"
   get '/deplos/prefecture/:id', to: "deplos#prefecture"
-  resources :deplos, only: [:index, :new, :show, :create, :edit]
+  resources :deplos, only: [:index, :new, :show, :create, :edit, :update]
 end
