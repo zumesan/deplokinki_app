4058ec40948d94b7d9e628e5d31dcd4f732708af
zumesan <zumehashi3240518@icloud.com>
2022-10-25 15:06:56 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/deplos/#{comment.deplo.id}"
+  end
+
+  def destroy
+    deplo = Deplo.find(params[:deplo_id])
+    comment = deplo.comments.find(params[:id])
+    if current_user == comment.user
+      comment.destroy
+      redirect_to "/deplos/#{comment.deplo.id}"
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment_content).merge(user_id: current_user.id, deplo_id: params[:deplo_id])
+  end
+
+end
