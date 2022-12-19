856cc5f684ae79b6abe842fd03f3813b0d7fca91
zumesan <zumehashi3240518@icloud.com>
2022-12-19 11:06:37 +0900

    管理者によるdeplo削除機能の実装

--- app/controllers/admin/deplos_controller.rb
+++ app/controllers/admin/deplos_controller.rb
@@ -2,6 +2,20 @@ class Admin::DeplosController < ApplicationController
   before_action :if_not_admin
   before_action :set_deplo, only: [:show, :edit, :destroy]
 
+  def show
+    @comment = Comment.new
+    @comments = @deplo
+  end
+
+  def edit
+  end
+
+  def destroy
+    if @deplo.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def if_not_admin
     redirect_to root_path unless current_user.admin?
