1715959287c57b9216c963c32a74cbd513516daa
zumesan <zumehashi3240518@icloud.com>
2023-02-15 11:47:32 +0900

    spec/factoriesフォルダの生成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confimation、名字（全角）と名前（全角）、名字カナ（全角）と名前カナ（全角）、電話番号（半角数字）があれば登録できる' do
+        except(@user).to be_valid
+      end
+    end
+  end
 end
