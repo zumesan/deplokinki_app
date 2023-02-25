64bbe4f1bac2b5dc1ec02496c366dbff39939a95
zumesan <zumehashi3240518@icloud.com>
2023-02-25 10:40:00 +0900

    文字数制限のバリデーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ApplicationRecord
   FULL_WIDTH_KANA = "は全角カタカナで入力してください" #全角カナ文字のバリデーションのエラーメッセージを変数に格納
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/ #半角数字のバリデーションの値を変数に格納
 
-  validates :nickname, presence:true
+  validates :nickname, presence:true, length: { minimum: 1, maximum:10 }
   validates :password, format:{with: VALID_PASSWORD_REGEX, message:"は半角英数字で入力してください" }, allow_blank: true #半角英数のバリデーション
   # 全角文字のバリデーション
-  validates :first_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}, length: { minimum: 1, maximum:100 }
-  validates :last_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}, length: { minimum: 1, maximum:100 }
+  validates :first_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}, length: { minimum: 1, maximum:30 }
+  validates :last_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}, length: { minimum: 1, maximum:30 }
 
   # 全角カナ文字のバリデーション
-  validates :first_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}, length: { minimum: 1, maximum: 100 }#全角カナ文字のバリデーション
-  validates :last_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}, length: { minimum: 1, maximum: 100 }#全角カナ文字のバリデーション
+  validates :first_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}, length: { minimum: 1, maximum: 30 }#全角カナ文字のバリデーション
+  validates :last_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}, length: { minimum: 1, maximum: 30 }#全角カナ文字のバリデーション
 
   # 電話番号のバリデーション
   validates :phone_number, numericality: {message: "は半角数字で入力してください"}, format:{ with: VALID_PHONE_REGEX, allow_blank:true }
