53472583524e2cf05a71b0cd3cef56e21fb6a60e
zumesan <zumehashi3240518@icloud.com>
2023-02-25 10:32:34 +0900

    コメントの文字数制限のバリデーションの記述

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :deplo 
 
-  validates :comment_content, presence: true
+  validates :comment_content, length: { minimum: 1, maximum: 140 }
 end
